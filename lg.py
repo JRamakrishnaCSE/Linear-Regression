071cce58eebb82d2cfd83f9637789763b128b389
mohneesh7 <mohneesh0@gmail.com>
2017-10-19 17:44:57 +0530

    added reading from a csv file and rms error and plotting function

--- lg.py
+++ lg.py
@@ -1,5 +1,17 @@
 import math as m
 import matplotlib.pyplot as p
+import csv
+import operator as o
+import math as m
+
+
+def csvread(a):
+	"""This is to read any dataset from a csv file"""
+	with open(a,'rb') as f:
+		row = csv.reader(f,delimiter='\t')				# set whatever the delimiter you want(',' or '\t')
+		l = lambda row : [map(float,x) for x in row]
+		return l(row)
+
 def mean(values):
 	"""Mean of the iterable."""
 	return (sum(values)/float(len(values)))
@@ -31,8 +43,8 @@ the model(calculates the coefficients) and the prediction is done on the test se
 	predictions = []
 	a,b = coeff(train)
 	for row in test:
-		yhat = b + a * row
-		predictions.append(yhat)
+		y = b + a * row
+		predictions.append(y)
 	return predictions
 
 def test_set(dataset):
@@ -40,13 +52,37 @@ def test_set(dataset):
 	test=[row[0] for row in dataset]
 	return test
 
-dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
-x=[r[0] for r in dataset]
-y=[r[1] for r in dataset]
-print coeff(dataset)
-test = test_set(dataset)
-a=simple_linear_regression(dataset,test)
-# p.plot(x,y)
-print a
-p.plot(x,y)
-p.show()
+def plot_act_vs_pre(x,y1,y2):
+	p.scatter(x,y1)
+	p.plot(x,y2)
+	p.show()
+
+def rmse(y1,y2):
+	a=map(o.__sub__,y2,y1)
+	a=[x**2 for x in a]
+	a=sum(a)/len(a)
+	a=m.sqrt(a)
+	return a
+
+def main():
+	"""This is the starting point of the program. \nJust put the desired filename below \nBy default it is 1.csv"""
+	file_name='1.csv'
+	dataset=csvread(file_name)
+	test=test_set(dataset)				# To check using the same dataset,for user defined values use below line(uncommented)
+
+	# test=[5,6,7]							# can give external testset to predict y values for, just comment out above line
+
+	predictions = simple_linear_regression(dataset,test)
+
+	x=[x[0] for x in dataset]
+	y1=[y[1] for y in dataset]
+	y2=predictions
+	plot_act_vs_pre(x,y1,y2)
+
+	print "The predicted values are :\n"
+	for i in predictions:
+		print i
+	print "\nThe RMS Error is :",rmse(y1,y2)
+	 
+main()
+
